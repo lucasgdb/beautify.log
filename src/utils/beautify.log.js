d22e1a1c832332a61042600a0dc49c1e90342f65
Lucas Bittencourt <lucasnaja0@gmail.com>
2020-01-21 17:26:10 -0300

    code improved

--- src/utils/beautify.log.js
+++ src/utils/beautify.log.js
@@ -4,15 +4,55 @@ const beautify = {
 			let str = string;
 
 			const texts = [
-				'reset', 'bright', 'dim', 'underscore', 'blink', 'reverse', 'hidden', 'fgBlack',
-				'fgRed', 'fgGreen', 'fgYellow', 'fgBlue', 'fgMagenta', 'fgCyan', 'fgWhite', 'bgBlack',
-				'bgRed', 'bgGreen', 'bgYellow', 'bgBlue', 'bgMagenta', 'bgCyan', 'bgWhite',
+				'reset',
+				'bright',
+				'dim',
+				'underscore',
+				'blink',
+				'reverse',
+				'hidden',
+				'fgBlack',
+				'fgRed',
+				'fgGreen',
+				'fgYellow',
+				'fgBlue',
+				'fgMagenta',
+				'fgCyan',
+				'fgWhite',
+				'bgBlack',
+				'bgRed',
+				'bgGreen',
+				'bgYellow',
+				'bgBlue',
+				'bgMagenta',
+				'bgCyan',
+				'bgWhite',
 			];
 
 			const codes = [
-				'\x1b[0m', '\x1b[1m', '\x1b[2m', '\x1b[4m', '\x1b[5m', '\x1b[7m', '\x1b[8m', '\x1b[30m',
-				'\x1b[31m', '\x1b[32m', '\x1b[33m', '\x1b[34m', '\x1b[35m', '\x1b[36m', '\x1b[37m', '\x1b[40m',
-				'\x1b[41m', '\x1b[42m', '\x1b[43m', '\x1b[44m', '\x1b[45m', '\x1b[46m', '\x1b[47m',
+				'\x1b[0m',
+				'\x1b[1m',
+				'\x1b[2m',
+				'\x1b[4m',
+				'\x1b[5m',
+				'\x1b[7m',
+				'\x1b[8m',
+				'\x1b[30m',
+				'\x1b[31m',
+				'\x1b[32m',
+				'\x1b[33m',
+				'\x1b[34m',
+				'\x1b[35m',
+				'\x1b[36m',
+				'\x1b[37m',
+				'\x1b[40m',
+				'\x1b[41m',
+				'\x1b[42m',
+				'\x1b[43m',
+				'\x1b[44m',
+				'\x1b[45m',
+				'\x1b[46m',
+				'\x1b[47m',
 			];
 
 			for (let i = 0; i < texts.length; i++) {
@@ -22,7 +62,11 @@ const beautify = {
 			return str;
 		});
 
-		strings[strings.length - 1] = `${strings[strings.length - 1]}\x1b[0m`;
+		if (strings.length) {
+			strings[strings.length - 1] = `${
+				strings[strings.length - 1]
+			}\x1b[0m`;
+		}
 
 		console.log(...strings);
 	},
